e61fe0910ad830425bcafd185ea743c7216aaef9
Vijay Sutar <vjsutar21@gmail.com>
2022-04-24 13:13:11 +0530

    Hulu clone

---
+++ utils/requests.js
@@ -0,0 +1,33 @@
+const API_KEY = process.env.API_KEY;
+
+const requests = {
+    fetchTrending: {
+        title: 'Trending',
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
+    },
+    fetchTopRated: {
+        title: 'Top Rated',
+        url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
+    },
+    fetchActionMovies: {
+        title: 'Action',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
+    },
+    fetchHorrerMovies: {
+        title: 'Horrer',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
+    },
+    fetchRomanceMovies: {
+        title: 'Romance',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
+    },
+    fetchComedyMovies: {
+        title: 'Comedy',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
+    },
+    fetchMystery: {
+        title: 'Mystery',
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
+    },
+}
+export default requests;
